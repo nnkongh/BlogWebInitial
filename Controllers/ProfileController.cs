874669cc193a5e3efb23f1e43832be7d827c58a9
nnkongh <haukong1308@gmail.com>
2025-01-16 12:17:06 +0700

    commit profile

--- Controllers/ProfileController.cs
+++ Controllers/ProfileController.cs
@@ -14,6 +14,7 @@ namespace BlogWeb.Controllers
     //    private readonly UserManager<User> _user;
     //    private readonly RoleManager<User> _role;
 
+<<<<<<< Updated upstream
     //    public ProfileController(UserManager<User> user, RoleManager<User> role) {
     //        _user = user;
     //        _role = role;
@@ -34,4 +35,48 @@ namespace BlogWeb.Controllers
     //        return View(model);
     //    }
     //}
+=======
+        public ProfileController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Profile()
+        {
+            var userId = _userManager.GetUserId(HttpContext.User);
+            if (userId == null) {
+                return RedirectToAction("Login","Account");
+            }
+            var user = await _userManager.FindByIdAsync(userId);
+            var model = new UserViewModel
+            {
+
+                UserName = user.UserName,
+                Email = user.Email,
+                Role = user.Role,
+            };
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Profile(UserViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {   
+                return View(model);
+            }
+            var userId = _userManager.GetUserId(HttpContext.User);
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null) { 
+                return NotFound();
+            }
+            user.Email = model.Email;
+            var updateUser = await _userManager.UpdateAsync(user);
+            if (updateUser.Succeeded) {
+                ViewBag.Message = "Profile updated successfully";
+                return View(model);
+            }
+            return View(model);
+        }
+    }
+>>>>>>> Stashed changes
 }
